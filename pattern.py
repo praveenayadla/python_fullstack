b1c0f128f0b6ec675e20a1e2122b918029262a86
praveena <praveenayadla066@gmail.com>
2026-07-22 15:29:10 +0530

    portfolio

---
+++ pattern.py
@@ -0,0 +1,64 @@
+# n=5
+# for i in range(n):
+#     for j i range(n):
+#     print("*",end=" ")
+#     print()
+# #inverted right angled traingle
+# n=5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #first how many rows
+# #second how many spaces
+# #third how many stars
+
+# #armstrong number
+# 153=1^3 + 5^3 +3^3
+# n=int(input("enter a number:"))
+# sum=0
+# temp = n
+# while temp>0:
+#     digit = temp%10
+#     sum += digit**3
+#     temp //= 10
+# if sum == n:
+#     print(n,"is a Armstrong number")
+# else:
+#     print(n,"is not a Armstrong number")
+    
+    
+# # hollow square pattern
+"""
+      
+            
